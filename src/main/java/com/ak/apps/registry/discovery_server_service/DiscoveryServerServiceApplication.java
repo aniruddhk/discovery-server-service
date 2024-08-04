f8720ca7afb042d19c8d5d4fca9dab257c6fa484
aniruddhku <aniruddhkulkarni.kulkarni@gmail.com>
2024-08-04 16:35:33 +0100

    initial commit

---
+++ src/main/java/com/ak/apps/registry/discovery_server_service/DiscoveryServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ak.apps.registry.discovery_server_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerServiceApplication.class, args);
+	}
+
+}
